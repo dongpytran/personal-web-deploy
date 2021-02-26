d2691a4937f951824066904cfcda774a28d565d6
Dong Py Tran <67514451+dongpytran@users.noreply.github.com>
2021-02-27 00:00:37 +0700

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -3,11 +3,19 @@ import "./Footer.css"
 function Footer() {
     const date = new Date();
     const year = date.getFullYear();
+    useEffect(()=>{
+        const count = document.getElementById('count');
+        countapi.update('dongpy.tk', 'viewer', 1).then((result) => { count.innerHTML = result.value});
+    }, [])
+
+
+
     return (
         <div className="footer">
             <p>Copyright &copy; {year} </p>
             <p>Developed by TranDongPy</p>
             <p>All rights reserved</p>
+            <p id="count" style={{color:"red"}}></p><span>visited</span>
         </div>
     )
 }
