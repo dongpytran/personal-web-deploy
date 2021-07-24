c6b56493a5b4823e74436aca2a148308cb880a6c
Dong Py Tran <67514451+dongpytran@users.noreply.github.com>
2021-07-24 13:24:59 +0700

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -27,7 +27,6 @@ function Header() {
             </div>
             <div className="header__nav">
                 <ul>
-                    <li><a href=".">Home</a></li>
                     <li><a href="#about">About</a></li>
                     <li><a href="#skills">Skills</a></li>
                     <li><a href="#projects">Projects</a></li>
